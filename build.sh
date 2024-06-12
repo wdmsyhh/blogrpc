7df68ced8af07478adf03161e771a5f4c7d91e1a
gary.yin <gary.yin@verystar.cn>
2024-06-12 16:25:59 +0800

    refine

--- build.sh
+++ build.sh
@@ -29,3 +29,30 @@ usage() {
 }
 
 main $@
+
+
+########################################################################################################################
+#docker build -t member:v1  -f ./service/member/Dockerfile .
+#
+#docker build -t hello:v1  -f ./service/hello/Dockerfile .
+#
+#docker build -t business:v1 -f ./openapi/business/Dockerfile .
+
+
+
+# 注意：如果打包成镜像，代码中所有访问另一个容器的地方，比如：localhost:8081 需要改成 ${容器别名}:${容器内部端口}
+#
+# docker network create my_default
+#
+# docker run -it --rm --name businesstest -p 8080:8080 --network my_default --network-alias business business:v1
+#
+# docker run -it --rm --name hellotest -p 8081:8081 --network my_default --network-alias hello hello:v1
+#
+# docker run -it --rm --name membertest -p 8082:8082 --network my_default --network-alias member member:v1
+#
+#
+# docker run -itd --name mongotest -p 27012:27017 --network my_default --network-alias mongo mongo:latest --auth
+# docker exec -it mongotest mongo admin
+# db.createUser({ user:'admin',pwd:'123456',roles:[ { role:'userAdminAnyDatabase', db: 'admin'},"readWriteAnyDatabase"]});
+# db.auth('admin', '123456')
+########################################################################################################################
